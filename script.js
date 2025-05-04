108402b8f2d0bd49e3d76cc2fe25d8eef9952fcd
AbdoooElfata7 <lpl66448844@gmail.com>
2025-05-04 13:33:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 
-function downloadFile() {
-    const link = document.createElement("a");
-    link.href = "Ahmed Abdel Fatah Fathi CV.docx"; // مسار الملف
-    link.download = "Ahmed Abdel Fatah Fathi CV"; // اسم الملف عند التحميل
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
-
